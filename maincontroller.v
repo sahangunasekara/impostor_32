c6be77882f79ce2b0331c05bc959bf8dcf33e4ca
SahanKG <skgunasekara99@gmail.com>
2023-02-11 00:33:44 +0530

    controller

--- maincontroller.v
+++ maincontroller.v
@@ -1,4 +1,4 @@
-module mainController( control_signal, Opcode, Aluop);
+module mainController( control_signal, Opcode, Aluop, RegWrite, MemWrite, MemRead, Branch, ALUSrc, Alu0, Alu1,jump, ALU_En, mem_reg);
 	input [6:0] Opcode;
 	output [1:0] Aluop;//ALUSrc,MemtoReg,RegWrite, MemRead, MemWrite, Branch,jump,
 	output reg [9:0] control_signal;
@@ -8,19 +8,15 @@ module mainController( control_signal, Opcode, Aluop);
 	always @(*)
 	begin
 		case(Opcode)
-			7'b0110011 : control_signal <= 8'b1000000000; // R-type
-			7'b0000011 : control_signal <= 8'b1010110000; // I-type load
-			7'b0010011 : control_signal <= 8'b1000101000; // I-type imm
-			7'b0100011 : control_signal <= 8'b0100110001; // s-type
-			7'b1100011 : control_signal <= 8'b0001011000; // sb-type
-			default : control_signal    <= 8'bxxxxxxxxxx;
-		endcase
-		
-		case(Opcode)
-			7'b1101111 : control_signal <= 8'b10001xx110; // jal-type
-			7'b0110111 : control_signal <= 8'b10001xx010; // lui-type
-			7'b0010111 : control_signal <= 8'b00000xx010; // auipc-type
-			default : control_signal    <= 8'bxxxxxxxxxx;
+			7'b0110011 : control_signal <= 10'b1000000000; // R-type
+			7'b0000011 : control_signal <= 10'b1010110000; // I-type load
+			7'b0010011 : control_signal <= 10'b1000101000; // I-type imm
+			7'b0100011 : control_signal <= 10'b0100110001; // s-type
+			7'b1100011 : control_signal <= 10'b0001011000; // sb-type
+			7'b1101111 : control_signal <= 10'b10001xx110; // jal-type
+			7'b0110111 : control_signal <= 10'b10001xx010; // lui-type
+			7'b0010111 : control_signal <= 10'b00000xx010; // auipc-type
+			default : control_signal    <= 10'bxxxxxxxxxx;
 		endcase
 		
 	end
