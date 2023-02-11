dac9ccb1622d2e1dfde903c7f4bf7bd9edb44455
SahanKG <skgunasekara99@gmail.com>
2023-02-11 13:25:56 +0530

    data memory

--- data_memory.v
+++ data_memory.v
@@ -2,22 +2,20 @@ module data_memory (addr, write_data, read_data, clk, mem_read, mem_write, DATA_
 
    input [31:0] addr;
    input [31:0] write_data;
+   input clk, mem_read, mem_write;
+   input [2:0] DATA_MEM_In;
    output reg [31:0] read_data;
 
-   input clk, mem_read, mem_write, signed_unsigned;
-
-   input [2,0] DATA_MEM_In;
-
-   reg [31:0] dmemory [63:0];
+  reg [31:0] dmemory [64:0];
    reg [31:0] write_data_in;
 
-   wire [5:0] shifted_addr;
+  wire [5:0] shifted_addr;
    wire [2:0] DATA_MEM_In;
    wire [31:0] DATA;
 
-   assign shifted_addr = addr[5:0];
+  assign shifted_addr = addr[5:0];
 
-   assign DATA = (mem_read) ? dmemory[addr] : 32'bx;
+   //assign DATA = dmemory[addr];
 
    always @(posedge clk)
    begin
@@ -27,18 +25,17 @@ module data_memory (addr, write_data, read_data, clk, mem_read, mem_write, DATA_
 
                case(DATA_MEM_In)
                3'b001: begin
-                   read_data[7:0] = DATA[7:0];
-                   read_data[31:8] = DATA[7]?24'b111111111111111111111111 : 8'b0 ;
+                 read_data = $signed(dmemory[shifted_addr][7:0]);
+   
                    end
 
                3'b010:begin
-                   read_data[15:0] = DATA[15:0];
-                   read_data[31:15] = DATA[15]?16'b1111111111111111 : 8'b0 ;
+                 read_data = $signed(dmemory[shifted_addr][15:0]);
                    end
 
-               3'b011: read_data = DATA;
-               3'b101: read_data = {24'b0, DATA[7:0]};
-               3'b110: read_data= {16'b0, DATA[15:0]};
+               3'b011: read_data = dmemory[shifted_addr];
+                 3'b101: read_data = $unsigned(dmemory[shifted_addr][7:0]);
+                 3'b110: read_data= $unsigned(dmemory[shifted_addr][15:0]);
                endcase
 
            end
@@ -49,16 +46,16 @@ module data_memory (addr, write_data, read_data, clk, mem_read, mem_write, DATA_
 
                case(DATA_MEM_In)
                3'b001: begin
-                   write_data_in = {24'b0, write_data[7:0]};
-                   dmemory[addr] = write_data_in;
+                 write_data_in = $unsigned(write_data[7:0]);
+                   dmemory[shifted_addr] = write_data_in;
                end
                3'b010:begin
-                   write_data_in = {16'b0, write_data[15:0]};
-                   dmemory[addr] = write_data_in;
+                 write_data_in = $unsigned(write_data[15:0]);
+                   dmemory[shifted_addr] = write_data_in;
                end
 
                3'b011:begin
-                   dmemory[addr] = write_data;
+                   dmemory[shifted_addr] = write_data;
                end
                endcase
            end
